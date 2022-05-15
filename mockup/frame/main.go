106019704f88d34b3099731ecef37f68a841a646
teleivo <teleivo@users.noreply.github.com>
2022-05-15 16:11:54 +0200

    chore: add frame mockup

---
+++ mockup/frame/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+	"golang.org/x/term"
+)
+
+const (
+	// In real life situations we'd adjust the document to fit the width we've
+	// detected. In the case of this example we're hardcoding the width, and
+	// later using the detected width only to truncate in order to avoid jaggy
+	// wrapping.
+	width = 96
+)
+
+// Style definitions.
+var (
+
+	// General.
+
+	highlight = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
+
+	// Tabs.
+
+	activeTabBorder = lipgloss.Border{
+		Top:         "─",
+		Bottom:      " ",
+		Left:        "│",
+		Right:       "│",
+		TopLeft:     "╭",
+		TopRight:    "╮",
+		BottomLeft:  "┘",
+		BottomRight: "└",
+	}
+
+	tabBorder = lipgloss.Border{
+		Top:         "─",
+		Bottom:      "─",
+		Left:        "│",
+		Right:       "│",
+		TopLeft:     "╭",
+		TopRight:    "╮",
+		BottomLeft:  "┴",
+		BottomRight: "┴",
+	}
+
+	tab = lipgloss.NewStyle().
+		Border(tabBorder, true).
+		BorderForeground(highlight).
+		Padding(0, 1)
+
+	activeTab = tab.Copy().Border(activeTabBorder, true)
+
+	tabGap = tab.Copy().
+		BorderTop(false).
+		BorderLeft(false).
+		BorderRight(false)
+
+	// Status Bar.
+
+	statusNugget = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFFDF5")).
+			Padding(0, 1)
+
+	statusBarStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.AdaptiveColor{Light: "#343433", Dark: "#C1C6B2"}).
+			Background(lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#353533"})
+
+	statusStyle = lipgloss.NewStyle().
+			Inherit(statusBarStyle).
+			Foreground(lipgloss.Color("#FFFDF5")).
+			Background(lipgloss.Color("#FF5F87")).
+			Padding(0, 1).
+			MarginRight(1)
+
+	encodingStyle = statusNugget.Copy().
+			Background(lipgloss.Color("#A550DF")).
+			Align(lipgloss.Right)
+
+	statusText = lipgloss.NewStyle().Inherit(statusBarStyle)
+
+	fishCakeStyle = statusNugget.Copy().Background(lipgloss.Color("#6124DF"))
+
+	// Page.
+
+	docStyle = lipgloss.NewStyle().Padding(1, 2, 1, 2)
+)
+
+func main() {
+	physicalWidth, _, _ := term.GetSize(int(os.Stdout.Fd()))
+	doc := strings.Builder{}
+
+	// Tabs
+	{
+		row := lipgloss.JoinHorizontal(
+			lipgloss.Top,
+			activeTab.Render("Stacks"),
+			tab.Render("Instances"),
+		)
+		gap := tabGap.Render(strings.Repeat(" ", max(0, width-lipgloss.Width(row)-2)))
+		row = lipgloss.JoinHorizontal(lipgloss.Bottom, row, gap)
+		doc.WriteString(row + "\n\n")
+	}
+
+	// Status bar
+	{
+		w := lipgloss.Width
+
+		statusKey := statusStyle.Render("STATUS")
+		encoding := encodingStyle.Render("UTF-8")
+		fishCake := fishCakeStyle.Render("🍥 Fish Cake")
+		statusVal := statusText.Copy().
+			Width(width - w(statusKey) - w(encoding) - w(fishCake)).
+			Render("Ravishing")
+
+		bar := lipgloss.JoinHorizontal(lipgloss.Top,
+			statusKey,
+			statusVal,
+			encoding,
+			fishCake,
+		)
+
+		doc.WriteString(statusBarStyle.Width(width).Render(bar))
+	}
+
+	if physicalWidth > 0 {
+		docStyle = docStyle.MaxWidth(physicalWidth)
+	}
+
+	// Okay, let's print it
+	fmt.Println(docStyle.Render(doc.String()))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
